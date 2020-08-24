c720dabf794a8e3de8b124518dd9fb0030092b26
Paul Jacobs <paul.jacobs@percona.com>
2020-08-24 16:49:07 +0300

    Move edit links up

--- source/conf.py
+++ source/conf.py
@@ -154,9 +154,9 @@ html_static_path = ['_static']
 
 # Custom sidebar templates, maps document names to template names.
 html_sidebars = { '**': [
+    'edit.html',
     'localtoc.html',
-    'relations.html',
-    'edit.html'
+    'relations.html'
     ] }
 
 # Additional templates that should be rendered to pages, maps page names to
