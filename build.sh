e7001a1e88aa3f79eb2b32e815700e8659541b9d
Alex Tymchuk <alexander.tymchuk@percona.com>
2022-02-01 00:28:13 +0300

    ci: fix the @ command prefix

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
-@echo "Starting the build process..."
+echo "Starting the build process..."
 
 mkdir -p dist
 wget -O ./themed.zip https://storage.googleapis.com/doc-builds/main/themed.zip
 
 cd dist
 unzip ../themed.zip
-@echo "End of the build process."
+echo "End of the build process."
